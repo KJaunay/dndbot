4b3ea1b3b517ac3e7eea905cc647370ccdfbc268
Kieran Jaunay <KJaunay@users.noreply.github.com>
2021-09-06 11:11:57 +0000

    start to add command definitions

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import discord
 import os
+from discord.ext import commands
+from replit import db
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command(name='dnd', help='Displays a welcome message with an example of how to use this bot') 
+async def dnd(ctx):
+  response="Hello! I am D&DBot. I can help you manage your campaign. Try typing !help to show the commands that are available. Each command will have a subcommand such as glossary add <person> <description>"
+  await ctx.send(response)
+
+@bot.command(name='glossary', help='Displays descriptions of people, places or things')
+async def glossary(ctx):
+  response="This is the glossary"
+  await ctx.send(response)
 
 
-client = discord.Client()
+'''
+#client = discord.Client()
 
 @client.event
 async def on_ready():
   print("logged in as {0.user}".format(client))
+
+
 
 @client.event
 async def on_message(message):
@@ -16,6 +37,10 @@ async def on_message(message):
   msg = message.content
   if msg.startswith("!hello"):
     await message.channel.send("Hello there!")
-    
+
+  if msg.startswith("!dwarf"):
+    await message.channel.send(db["dwarf"])
+''' 
+
 token = os.environ['DISCORDBOTTOKEN1']
-client.run(token)
+bot.run(token)
